20532ee559f263936aa50bd6193eae47969da082
KallDrexx <me@mshapiro.net>
2025-07-17 21:53:27 -0400

    Fix gradle build file

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,8 +31,8 @@ repositories {
 
 // Dependencies are managed with Gradle version catalog - read more: https://docs.gradle.org/current/userguide/platforms.html#sub:version-catalog
 dependencies {
-    testImplementation(libs.junit)
-    testImplementation(libs.opentest4j)
+//    testImplementation(libs.junit)
+//    testImplementation(libs.opentest4j)
 
     // IntelliJ Platform Gradle Plugin Dependencies Extension - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-dependencies-extension.html
     intellijPlatform {
